dd4e0c1d748dd0a268c928ca1f29b3d2cd001d11
huy8856 <thai.giahuy123@gmail.com>
2023-12-08 00:13:52 +0700

    Fix directory so it can understand and read dist correctly

--- server/config/cloudinary.config.ts
+++ server/config/cloudinary.config.ts
@@ -1,6 +1,7 @@
 import { v2 as cloudinary } from "cloudinary"
 import { CloudinaryStorage, Options } from "multer-storage-cloudinary"
 import multer, { Multer } from "multer"
+require("dotenv").config()
 
 // Configure Cloudinary
 cloudinary.config({
