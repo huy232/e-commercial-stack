35264bf110070036e6ec3e35d58b61c87ae95ea4
huy8856 <thai.giahuy123@gmail.com>
2023-12-22 21:51:11 +0700

    Add MailNotify

---
+++ client/app/components/MailNotify/index.tsx
@@ -0,0 +1,14 @@
+const MailNotify = () => {
+	return (
+		<div className="flex">
+			<div>
+				<h3>SIGN UP TO NEWSLETTER</h3>
+				<p>Subscribe now and receive weekly newsletter</p>
+			</div>
+			<div>
+				<input type="text" id="mail" placeholder="Email address" />
+			</div>
+		</div>
+	)
+}
+export default MailNotify
